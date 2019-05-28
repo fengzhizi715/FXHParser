af0f7509cd94ec06292938ab5b2109e3e7353d7a
fengzhizi715 <fengzhizi715@126.com>
2019-05-28 21:00:14 +0800

    update FXHParser

--- src/main/java/cn/netdiscovery/fxh/parser/FXHParser.java
+++ src/main/java/cn/netdiscovery/fxh/parser/FXHParser.java
@@ -17,18 +17,12 @@ public class FXHParser implements Parser {
     @Override
     public void process(Page page) {
 
-//        ResultItems resultItems = page.getResultItems();
-//
-//        String price = page.getHtml().xpath("//*[@id=\"__layout\"]/section/div/div/div[1]/div[1]/div[2]/div[1]/div[1]/span/span").get();
-//        price = price.replace("<span class=\"convert\">","").replace("</span>","");
-//        resultItems.put("price",price);
-//
-//        List<Exchange> exchangeList = new ArrayList<>();
-//
-//        String list = page.getHtml().xpath("//*[@id=\"__layout\"]/section/div/div/div[1]/div[2]/div/div[2]/div[3]/div/div/div[2]/table/tbody/tr[1]/td[4]/div/span").get();
-//
-//        System.out.println(list);
+        ResultItems resultItems = page.getResultItems();
 
-        System.out.println(page.getHtml().get());
+        String price = page.getHtml().xpath("//*[@id=\"__layout\"]/section/div/div/div[1]/div[1]/div[2]/div[1]/div[1]/span/span").get();
+        price = price.replace("<span class=\"convert\">","").replace("</span>","");
+        resultItems.put("price",price);
+
+        List<Exchange> exchangeList = new ArrayList<>();
     }
 }
