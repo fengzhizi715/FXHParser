1343af8ec5ec30d2a75871efd7f2869654657cfe
fengzhizi715 <fengzhizi715@126.com>
2019-05-30 13:20:15 +0800

    update Main

--- src/main/java/cn/netdiscovery/fxh/Main.java
+++ src/main/java/cn/netdiscovery/fxh/Main.java
@@ -2,7 +2,6 @@ package cn.netdiscovery.fxh;
 
 import cn.netdiscovery.core.Spider;
 import cn.netdiscovery.core.SpiderEngine;
-import cn.netdiscovery.core.queue.disruptor.DisruptorQueue;
 import cn.netdiscovery.fxh.parser.FXHParser;
 
 /**
@@ -12,14 +11,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        DisruptorQueue queue = new DisruptorQueue();
-
         SpiderEngine engine = SpiderEngine.create();
 
-        engine.addSpider(Spider.create(queue)
+        Spider spider = Spider.create()
                 .name("bitcoin")
                 .url("https://dncapi.bqiapp.com/api/coin/market_ticker?page=1&pagesize=10&code=bitcoin&token=&webp=1")
-                .parser(new FXHParser()))
+                .parser(new FXHParser());
+
+        engine.addSpider(spider)
                 .run();
     }
 }
